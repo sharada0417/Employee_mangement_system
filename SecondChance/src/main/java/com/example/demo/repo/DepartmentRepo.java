8b51844a415b1046777a1dc76417522274320d36
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-31 00:05:44 +0530

    update department services

--- SecondChance/src/main/java/com/example/demo/repo/DepartmentRepo.java
+++ SecondChance/src/main/java/com/example/demo/repo/DepartmentRepo.java
@@ -8,7 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepo extends JpaRepository<Department,Integer>{
-
-   
-
+	
 }
--- SecondChance/src/main/java/com/example/demo/service/DepartmentService.java
+++ SecondChance/src/main/java/com/example/demo/service/DepartmentService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.model.Department;
 import com.example.demo.repo.DepartmentRepo;
 
+import jakarta.persistence.EntityNotFoundException;
+
 public class DepartmentService {
 	@Autowired
 	private DepartmentRepo repo;
@@ -17,7 +19,8 @@ public class DepartmentService {
 	
 	public Department getDept(int id) {
 		if(repo.findById(id).isEmpty()) {
-			
+			throw new EntityNotFoundException("Department not found");
 		}
+		return repo.findById(id).get();
 	}
 }
