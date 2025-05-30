d15ed9abc431385002956afb558da2b4ad6c6cae
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-30 23:13:37 +0530

    create Error Response model;

--- SecondChance/src/main/java/com/example/demo/model/ErrorResponse.java
+++ SecondChance/src/main/java/com/example/demo/model/ErrorResponse.java
@@ -1,5 +1,36 @@
 package com.example.demo.model;
 
 public class ErrorResponse {
+	private int statusCode;
+	public int getStatusCode() {
+		return statusCode;
+	}
 
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	private String errorMessage;
+	
+	public ErrorResponse() {
+	}
+	
+	public ErrorResponse(int statusCode,String errorMessage) {
+		super();
+		this.statusCode = statusCode;
+		this.errorMessage = errorMessage; 
+	}
+	
+	
+	
+	
+	
 }
