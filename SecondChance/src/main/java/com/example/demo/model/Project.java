e9597d5c8ed52eafbb2cdbb1f12a6ad44679243b
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-30 21:52:57 +0530

    create project Model

--- SecondChance/src/main/java/com/example/demo/model/Project.java
+++ SecondChance/src/main/java/com/example/demo/model/Project.java
@@ -1,5 +1,22 @@
 package com.example.demo.model;
 
-public class Project {
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Project {
+	@Id
+	private int id;
+	private String name;
+	private long totalcost;
+	@ManyToMany
+	@JoinTable(name="project_assignment",
+	joinColumns = @JoinColumn(name="pro_id"),
+	inverseJoinColumns = @JoinColumn(name="emp_id"))
+	private List<Employee> employees;
 }
