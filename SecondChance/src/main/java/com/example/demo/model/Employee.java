596e32451c2e9131cd35b6b2771d8d33b46f306e
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-30 21:59:05 +0530

    create Employee model

--- SecondChance/src/main/java/com/example/demo/model/Employee.java
+++ SecondChance/src/main/java/com/example/demo/model/Employee.java
@@ -1,5 +1,19 @@
 package com.example.demo.model;
 
-public class Employee {
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Employee {
+	@Id
+	private String empNo;
+	private double salary;
+	@ManyToOne
+	private Department department;
+	@ManyToMany(mappedBy="employees")
+	private List<Project>projects;
 }
