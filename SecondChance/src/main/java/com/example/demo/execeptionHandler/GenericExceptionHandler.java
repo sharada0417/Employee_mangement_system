eafa9786d15503fe441ff1d833a0b0aedeac14ba
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-30 23:53:25 +0530

    create Ge4neric Exception handler;

--- SecondChance/src/main/java/com/example/demo/execeptionHandler/GenericExceptionHandler.java
+++ SecondChance/src/main/java/com/example/demo/execeptionHandler/GenericExceptionHandler.java
@@ -1,5 +1,29 @@
 package com.example.demo.execeptionHandler;
 
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.demo.model.ErrorResponse;
+
+import jakarta.persistence.EntityNotFoundException;
+
 public class GenericExceptionHandler {
 
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFound(EntityNotFoundException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                exception.getMessage());
+        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateKey(DuplicateKeyException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                exception.getMessage());
+        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
