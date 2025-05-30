21db9d3050725fcfa6a888e11faf6b7fbdfcdbc5
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-30 23:39:45 +0530

    create departmentService;

--- SecondChance/src/main/java/com/example/demo/service/DepartmentService.java
+++ SecondChance/src/main/java/com/example/demo/service/DepartmentService.java
@@ -1,5 +1,23 @@
 package com.example.demo.service;
 
-public class DepartmentService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.model.Department;
+import com.example.demo.repo.DepartmentRepo;
+
+public class DepartmentService {
+	@Autowired
+	private DepartmentRepo repo;
+	
+	public List<Department> getDepts(){
+		return repo.findAll();
+	}
+	
+	public Department getDept(int id) {
+		if(repo.findById(id).isEmpty()) {
+			
+		}
+	}
 }
