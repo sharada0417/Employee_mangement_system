2c888d8751d810d387eb5878bcb7f22a096b7e9f
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-31 00:14:16 +0530

    create demo contro9ller

--- SecondChance/src/main/java/com/example/demo/controller/DepartmentController.java
+++ SecondChance/src/main/java/com/example/demo/controller/DepartmentController.java
@@ -1,5 +1,32 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Department;
+import com.example.demo.service.DepartmentService;
+
+@RestController
+@RequestMapping("/dept")
 public class DepartmentController {
 
+    @Autowired
+    public DepartmentService service;
+
+    @GetMapping
+    public ResponseEntity<List<Department>> getDepts() {
+        return new ResponseEntity<List<Department>>(service.getDepts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Department> getDept(@PathVariable("id") int id) {
+        return new ResponseEntity<Department>(service.getDept(id), HttpStatus.OK);
+    }
 }
