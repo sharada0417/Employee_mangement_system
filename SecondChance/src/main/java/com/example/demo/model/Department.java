8eb861e5a05836779dc3bd3821f10151fe74c3a7
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-30 22:31:04 +0530

    Department model

--- SecondChance/src/main/java/com/example/demo/model/Department.java
+++ SecondChance/src/main/java/com/example/demo/model/Department.java
@@ -1,5 +1,48 @@
 package com.example.demo.model;
 
-public class Department {
+import java.sql.Date;
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+public class Department {
+	@Id
+	@Column(name="dept_id")
+	private int id;
+	@Column(nullable = false)
+	private String name;
+	private Date established;
+	@OneToMany(mappedBy = "department")
+	private List<Employee> employees;
+	
+	public Department() {
+		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getEstablished() {
+		return established;
+	}
+	public void setEstablished(Date established) {
+		this.established = established;
+	}
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+	
 }
